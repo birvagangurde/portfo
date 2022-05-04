5e019bd317b1cb57c24fa570bc1db73f9bf0856c
birvagangurde <gangurdebirva@gmail.com>
2022-05-05 01:14:51 +0530

    my portfolio website

---
+++ server.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'hello world'
+
+@app.route('/')
+def my_home():
+    return  render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+# page_name as the input and print out below in render_template
+def html_page(page_name):
+    return  render_template(page_name)
+
+def write_to_file(data):
+    with open('./web/database.txt', mode='a') as database:
+        email = data("email")
+        subject = data("subject")
+        message = data("message")
+        file= database.write(f'\n{email}, {subject}, {message}')
+
+# BAP7
+def write_to_csv(data):
+    with open('./web/database.csv',newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',' ,quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message]) 
+
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong, try again!'
+
+
+# BAP4:-a basic infro of contact.html
+# @app.route('/submit_form', methods=['POST', 'GET'])
+# def login():
+#     return 'form submitted hoorayyy'
+
+
+
+# BAP6
+# @app.route('/submit_form', methods=['POST', 'GET'])
+# def submit_form():
+#     if request.method == 'POST':
+#         data = request.form.to_dict
+#         # print(data) in BAP6
+#         write_to_file(data)
+#         return redirect('/thankyou.html')
+#     else:
+#         return 'something went wrong. Try again'
+
+
+
